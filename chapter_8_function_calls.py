819a2ef2d0ab140d7c63b388d2eb2ad444621cf2
Michelle Banh <banh.mich@gmail.com>
2019-06-12 13:54:58 -0700

    Add additional exercises on Chapter 8 Functions

--- chapter_8_function_calls.py
+++ chapter_8_function_calls.py
@@ -1,7 +1,88 @@
 from chapter_8_module import *
 
+
 # 8-1
 display_message()
 
+
 # 8-2
 favorite_book("Ender's Game")
+
+
+# 8-3
+make_shirt("small", "I like cats") # call function using positional arguments
+make_shirt(text="Boba is life", size="large") # call function using keyword arguments
+
+
+# 8-4
+make_shirt()
+make_shirt(size="medium")
+make_shirt(size="small", text="This is a t-shirt")
+
+
+# 8-5
+describe_city("San Francisco")
+describe_city("Kyoto", "Japan")
+describe_city("Taipei", "Taiwan")
+
+
+# 8-6
+print(city_country("Sacramento", "U.S.A."))
+print(city_country("Penzberg", "Germany"))
+print(city_country("Basel", "Switzerland"))
+
+
+# 8-7
+print(make_album('Sam Tsui', 'Trust'))
+print(make_album('Tori Kelly', 'Unbrekaable Smile'))
+print(make_album('AJ Rafael', 'Red Roses'))
+print(make_album('AJ Rafael', 'Red Roses', 8))
+
+
+# 8-8
+while True:
+    print("\n" "I'd love to hear about one of your favorite albums!")
+    print("(Or you can enter 'q' at any time to quit)")
+    print("Please tell me...")
+
+    artist_name = input("What is the artist\'s name? ")
+    if artist_name == 'q':
+        break
+
+    album_title = input("What is the title of one of their albums? ")
+    if album_title == 'q':
+        break
+
+    print(make_album(artist_name, album_title))
+
+
+# 8-9
+print('\n' '8-9 The OG magicians')
+magicians = ["Houdini", "Dark Magician", "Harry Potter"]
+show_magicians(magicians)
+
+
+# 8-10
+print('\n' '8-10 The great magicians')
+make_great(magicians)
+show_magicians(magicians)
+
+
+# 8-11
+print('\n' '8-11 The OG and the great magicians again')
+great_magicians = make_great(magicians[:])
+show_magicians(magicians)
+print('')
+show_magicians(great_magicians)
+
+
+# 8-12
+make_sandwich('egg', 'meat')
+make_sandwich("ham", "cheese", "tomato")
+make_sandwich("avocado", "lettuce", "mayonnaise", "extra veggies")
+
+
+# 8-14
+car = make_car('subaru', 'outback', color='blue', tow_package=True)
+print(car)
+
--- chapter_8_module.py
+++ chapter_8_module.py
@@ -7,3 +7,65 @@ def favorite_book(title):
     """Displays one's favorite book given the title"""
     print(f"One of my favorite books is {title}.")
 
+
+# def make_shirt(size, text):
+#     """Displays summary (size and text) of t-shirt being made"""
+#     print(f'This shirt is a size {size}, and has the printed message, "{text}."')
+
+
+def make_shirt(size="large", text="I love Python"):
+    """Displays summary (size and text) of t-shirt being made"""
+    print(f'This shirt is a size {size}, and has the printed message, "{text}."')
+
+
+def describe_city(city, country="the U.S."):
+    """Displays the country that a city is in"""
+    print(f"{city.title()} is in {country}.")
+
+
+def city_country(city, country):
+    """Returns string of City, Country"""
+    return f"{city}, {country}"
+
+
+def make_album(artist_name, album_title, number_of_tracks=""):
+    """Returns dictionary of album information"""
+    album = {'artist name': artist_name, 'album title': album_title}
+
+    if number_of_tracks:
+        album['number of tracks'] = number_of_tracks
+
+    return album
+
+
+def show_magicians(magicians):
+    """Prints the same of each magician in a list"""
+    for magician in magicians:
+        print(f"{magician} is a magician! How magical!")
+
+
+def make_great(magicians):
+    """Adds 'the Great' to each magician in a list, and then returns the list"""
+    for i, magician in enumerate(magicians):
+        magician = magician + " the Great"
+        magicians[i] = magician
+    return magicians
+
+
+def make_sandwich(*sandwich_items):
+    """Displays summary of items in a sandwich"""
+    print("\n" "This sandwich has the following item(s):")
+    for item in sandwich_items:
+        print(f"- {item}")
+
+
+def make_car(manufacturer, model, **characteristics):
+    """Returns dictionary of car characteristics"""
+    car = {'manufacturer': manufacturer, 'model': model}
+    
+    for characteristic, value in characteristics.items():
+        car[characteristic] = value
+
+    return car
+
+
