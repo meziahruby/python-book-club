57826e81e8f856fe2fc60a2473e157c4736cffb5
Michelle Banh <banh.mich@gmail.com>
2019-06-12 00:01:43 -0700

    Add exercises from Chapter 8 Functions

---
+++ chapter_8_function_calls.py
@@ -0,0 +1,7 @@
+from chapter_8_module import *
+
+# 8-1
+display_message()
+
+# 8-2
+favorite_book("Ender's Game")
---
+++ chapter_8_module.py
@@ -0,0 +1,9 @@
+def display_message():
+    """Displays a simple message"""
+    print("I am learning about functions in this chapter!")
+
+
+def favorite_book(title):
+    """Displays one's favorite book given the title"""
+    print(f"One of my favorite books is {title}.")
+
