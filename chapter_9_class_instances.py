9d0cba7b8cefdfdd718f0ab3b368fc5f095d2850
Michelle Banh <banh.mich@gmail.com>
2019-06-21 23:42:05 -0700

    Add additional exercises from Chapter 9 Classes

--- chapter_9_class_instances.py
+++ chapter_9_class_instances.py
@@ -1,8 +1,61 @@
-from chapter_9_classes_module import Restaurant
+from chapter_9_classes_module import Restaurant, User
 
-# 9-1
-tasty_pot = Restaurant("Tasty Pot", "hotpot")
-print(tasty_pot.restaurant_name)
-print(tasty_pot.cuisine_type)
-tasty_pot.desecribe_restaurant()
-tasty_pot.open_restaurant()
+
+# # 9-1
+# tasty_pot = Restaurant("Tasty Pot", "hotpot")
+# print(tasty_pot.restaurant_name)
+# print(tasty_pot.cuisine_type)
+# tasty_pot.desecribe_restaurant()
+# tasty_pot.open_restaurant()
+
+
+# # 9-2
+# bonchon = Restaurant("Bonchon", "Korean")
+# bonchon.desecribe_restaurant()
+
+# marugame = Restaurant("Marugame", "Japanese")
+# marugame.desecribe_restaurant()
+
+# guads = Restaurant("Taqueria Guadalajara", "Mexican")
+# guads.desecribe_restaurant()
+
+
+# # 9-3
+# user_a = User("Jeremy", "Lin", 30, "male")
+# user_a.describe_user()
+# user_a.greet_user()
+
+# user_b = User("Taylor", "Swift", 25, "Female")
+# user_b.describe_user()
+# user_b.greet_user()
+
+# user_c = User("Alex", "Wong", 34, "NA")
+# user_c.describe_user()
+# user_c.greet_user()
+
+
+# # 9-4
+# restaurant_a = Restaurant("In 'n Out", "American")
+# print(restaurant_a.number_served)
+
+# restaurant_a.number_served = 20
+# print(restaurant_a.number_served)
+
+# restaurant_a.set_number_served(30)
+# print(restaurant_a.number_served)
+
+# restaurant_a.increment_number_served(-5)
+# print(restaurant_a.number_served)
+
+
+# 9-5
+user_d = User("Billy", "Bean", 15, "male")
+print(user_d.login_attempts) # 0
+
+user_d.increment_login_attempts()
+user_d.increment_login_attempts()
+user_d.increment_login_attempts()
+print(user_d.login_attempts) # 3
+
+user_d.reset_login_attempts()
+print(user_d.login_attempts) # 0
--- chapter_9_classes_module.py
+++ chapter_9_classes_module.py
@@ -1,14 +1,54 @@
 class Restaurant():
     """A simple restaurant"""
 
-    def __init__(self, restaurant_name, cuisine_type):
+    def __init__(self, restaurant_name, cuisine_type, number_served=0):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
 
     def desecribe_restaurant(self):
         print(f"{self.restaurant_name} has {self.cuisine_type} cuisine.")
 
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open!")
+
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, increment_number):
+        if increment_number < 0:
+            print("You can't increment the number of customers by a negative number!")
+        else:
+            self.number_served += increment_number
+
+
+class User():
+    """A simple user"""
+
+    def __init__(self, first_name, last_name, age, sex, login_attempts=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.sex = sex
+        self.login_attempts = login_attempts
+
+    def describe_user(self):
+        if self.sex.lower() == "male":
+            pronoun = "He"
+        elif self.sex.lower() == "female":
+            pronoun = "She"
+        else:
+            pronoun = "He/she"
+
+        print(f"This user's name is {self.first_name} {self.last_name}. {pronoun} is {self.age} year(s) old.")
+
+    def greet_user(self):
+        print(f"Hi, {self.first_name}! It's nice to meet you!")
+
+    def increment_login_attempts(self):
+        self.login_attempts +=1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 
