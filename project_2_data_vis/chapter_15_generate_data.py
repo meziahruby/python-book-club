da428ed287c27013e9440a1bf3a1fc8753b76d3b
Michelle Banh <banh.mich@gmail.com>
2019-07-08 08:34:14 -0700

    Add exercises on Chapter 15 Generating Data

--- project_2_data_vis/chapter_15_generate_data.py
+++ project_2_data_vis/chapter_15_generate_data.py
@@ -10,7 +10,9 @@ for index, x_max in enumerate(x_maxes):
 
     # Create figure in loop that will be shown in separate window after loop
     # figure index starts at 1
-    plt.figure(index + 1)
+    # plt.figure(index + 1)
+    # actually don't need to specify figure index
+    plt.figure()
 
     plt.scatter(x_data, y_data, c=y_data, cmap=plt.cm.Blues)
     
@@ -20,3 +22,8 @@ for index, x_max in enumerate(x_maxes):
     plt.axis([0, x_data[-1], 0, y_data[-1]])
 
 plt.show()
+
+
+# 15-3 Molecular Motion (skipped)
+# 15-4 Modified Random Walks (Adjusted direction and distance params to see effects on plots)
+# 15-5 Refactoring Random Walk with separate get_step() method
